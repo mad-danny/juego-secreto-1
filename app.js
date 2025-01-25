755521a603b847b6713e45756bde4f8c6a366ff1
mad-danny <danielac1971@gmail.com>
2025-01-25 16:08:23 -0300

    Version final juego numero secreto

---
+++ app.js
@@ -0,0 +1,62 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `¡Acertaste el número en ${intentos} ${intentos === 1 ? 'vez' : 'veces'}!`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        // El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * 10) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    
+    // Si el número generado ya está en la lista, vuelve a intentarlo
+    if (listaNumerosSorteados.includes(numeroGenerado)) {
+        return generarNumeroSecreto();
+    } else {
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1', '¡Juego del Número Secreto!');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(`Número secreto generado: ${numeroSecreto}`);
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+// Inicializa las condiciones del juego
+condicionesIniciales();
